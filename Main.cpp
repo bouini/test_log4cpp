6c4d02e82fe482f9b180f8fdeef1297fda511259
bouini <bouini@gmail.com>
2016-10-22 11:07:14 +0200

    add logger macros

--- Main.cpp
+++ Main.cpp
@@ -2,23 +2,38 @@
 
 #include "log4cpp/Category.hh"
 #include "log4cpp/Appender.hh"
-#include "log4cpp/FileAppender.hh"
 #include "log4cpp/OstreamAppender.hh"
 #include "log4cpp/Layout.hh"
-#include "log4cpp/BasicLayout.hh"
+#include "log4cpp/PatternLayout.hh"
 #include "log4cpp/Priority.hh"
 
-int main(int argc, char** argv)
-{
-    log4cpp::Appender *appender = new log4cpp::OstreamAppender("console", &std::cout);
-    appender->setLayout(new log4cpp::BasicLayout());
+#define GET_LOGGER(LOGGER) log4cpp::Category::getInstance((LOGGER))
+
+#define LOG_ERROR(LOGGER,MSG,...) GET_LOGGER(LOGGER).error((MSG),## __VA_ARGS__)
+#define LOG_WARN(LOGGER,MSG,...) GET_LOGGER(LOGGER).warn((MSG),## __VA_ARGS__)
+#define LOG_INFO(LOGGER,MSG,...) GET_LOGGER(LOGGER).info((MSG),## __VA_ARGS__)
+#define LOG_DEBUG(LOGGER,MSG,...) GET_LOGGER(LOGGER).debug((MSG),## __VA_ARGS__)
+#define LOG_VERBOSE(LOGGER,MSG,...) GET_LOGGER(LOGGER).verbose((MSG),## __VA_ARGS__)
+
+#define LOGGER_NAME "log"
+
+void init_logger() {
+    log4cpp::PatternLayout* layout = new log4cpp::PatternLayout();
+    layout->setConversionPattern("[%d{%Y/%m/%d %H:%M:%S,%l}] [%p] <%c> %m%n");
     
-    log4cpp::Category& root = log4cpp::Category::getRoot();
-    root.setPriority(log4cpp::Priority::INFO);
-    root.addAppender(appender);
+    log4cpp::Appender* appender = new log4cpp::OstreamAppender("console", &std::cout);
+    appender->setLayout(layout);
     
-    root.error("root error");
-    root.info("root info");
+    log4cpp::Category& logger = log4cpp::Category::getInstance(std::string(LOGGER_NAME));
+    logger.setPriority(log4cpp::Priority::ERROR);
+    logger.addAppender(appender);
+}
+
+int main(int argc, char** argv) {
+    init_logger();
+    
+    LOG_ERROR(LOGGER_NAME,"hello world!");
+    LOG_INFO(LOGGER_NAME,"HELLO WORLD!");
 
     return 0;
 }
